4153d687664e797ebb3c4853d011e3f3bcf8ea60
Luis <luiss18.code@gmail.com>
2025-03-24 19:59:48 -0300

    feat: update dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { theme, colors } from './styles/theme'
+import { Camera, Home, Settings, Bell } from "lucide-react";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-blue-500 text-4xl">Boa noite!</h1>
+      <h1 className="text-3xl " style={{ color: colors.red[200], }}>Boa noite!</h1>
+      <Camera className='w-10 h-10' />
     </>
   )
 }
