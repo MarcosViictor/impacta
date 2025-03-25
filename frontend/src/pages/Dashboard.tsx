5847145b01ecfa2b4a76ae5c81f75d69361fa91f
Luis <luiss18.code@gmail.com>
2025-03-24 22:15:04 -0300

    feat: add button component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,36 @@
+import { Button } from '@/components/Button'
 import { Header } from '@/components/Header'
+import { Link } from 'react-router-dom'
 
 export const Dashboard = () => {
   return (
     <>
         <Header />
+
+
+        {/* pode apagar isso */}
+
+        <div className="flex gap-4 w-[500px]">
+          <Button
+            as={Link} // para usar o link como um botão
+            to='/a' //rota para onde o botão vai
+            variant='primary'
+          
+            width='full'
+          >
+              Quero ajudar
+          </Button>
+          <Button
+            variant='secondary'
+            width='full'
+            className='text-amber-700'
+          
+          >
+              Teste
+          </Button>
+        </div>
+  
+      
     </>
   )
 }
