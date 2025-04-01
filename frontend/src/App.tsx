950a50ab53aff048fab6e163d3b4a2ae462f5cdb
Vinícius Lacerda Borges <132666308+viniciuslacerd4@users.noreply.github.com>
2025-04-01 12:54:13 -0300

    Add: FAQ + Cart Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Dashboard } from '@/pages/Dashboard';
 import { OngDetails } from '@/pages/OngDetails';
 import { UserProfile } from '@/pages/UserProfile';
+import { FAQPage } from './pages/FAQ';
+import ShoppingCart from './pages/Cart';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
           <Route path='/' element={<Dashboard />} />
           <Route path='/ong' element={<OngDetails />} />
           <Route path='/user' element={<UserProfile />} />
+          <Route path='/faq' element={<FAQPage />} />
+          <Route path='/cart' element={<ShoppingCart />} />
           
         </Routes>
       </BrowserRouter>
