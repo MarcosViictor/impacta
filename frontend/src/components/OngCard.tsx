44c64eb558157d3c8f5d54eb551b468f419f8053
Luis <luiss18.code@gmail.com>
2025-04-04 23:04:33 -0300

    feat: adicionar página de busca e remover componente de inputs

---
+++ frontend/src/components/OngCard.tsx
@@ -0,0 +1,52 @@
+import { MapPin } from "lucide-react"
+import { Button } from "./Button"
+
+interface OngCardProps {
+    name: string
+    description: string
+    city: string
+    state: string
+}
+
+export const OngCard = ( {name, description, city, state } : OngCardProps) => {
+    return (
+        <>
+            <div className="w-[400px]">
+
+                <div className="bg-gray-200 h-40 rounded-t-[10px]">
+
+                </div>
+
+                <div className="flex flex-col gap-2 border-2 border-gray-200 rounded-b-[10px] p-4">
+                    <h3 className="text-[1.5rem] font-bold">{name}</h3>
+                    <span className="text-gray-700 text-[.9rem]">{description}</span>
+                    <div className="flex gap-1.5 items-center text-gray-700">
+                        <span>
+                            <MapPin className="w-5 h-5"/>
+                        </span>
+                        <span className="text-[.9rem]">
+                            {city}, {state}
+                        </span>
+                    </div>
+
+                    <div className="flex justify-between pt-5">
+                        <Button
+                            variant="light"
+                            size="sm"
+                        >
+                            Ver detalhes
+                        </Button>
+
+                        <Button
+                            size="sm"
+                            className=""
+                        >
+                            <p className="px-3">Doar</p>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        
+        </>
+    )
+}
