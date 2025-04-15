23c1ee04066219bd07820f051faa9b1c6348b824
victor <victorfire99@gmail.com>
2025-04-14 22:56:40 -0300

    fix ajustes

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Enums para os campos que usam ENUM
 class AvaliationStatus(models.TextChoices):
     ENU = 'EN', _('EN')  
 
