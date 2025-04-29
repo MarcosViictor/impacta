6dcaa99fd5a212311cacf84dfc9a8f33237c04bd
victor <victorfire99@gmail.com>
2025-04-29 17:17:35 -0300

    update: models and user views were added, as well as registration and records via jwt

---
+++ backend/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from users.views.registerView import RegisterView  
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
