3699a85fc27f8b67a2510290e7542a0914c29d70
JotaEfi <joaodaviabc@gmail.com>
2025-05-19 02:50:11 -0300

    fix, feat: 'Nome padrão da ONG removido, ong é criada da mesma forma que usuário, e  endereço do usuário foi atribuído.

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import CustomUser, Ong
 
-@receiver(post_save, sender=CustomUser)
-def create_ong_profile(sender, instance, created, **kwargs):
-    if created and instance.user_type == 'ONG':
-        Ong.objects.create(user=instance, name='Nome padrão da ONG')
+# @receiver(post_save, sender=CustomUser)
+# def create_ong_profile(sender, instance, created, **kwargs):
+#     if created and instance.user_type == 'ONG':
+#         Ong.objects.create(user=instance, name='Nome padrão da ONG')
