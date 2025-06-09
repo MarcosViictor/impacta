17cb907d31eeeadbd589ecbbce386e41b38047f4
JotaEfi <joaodaviabc@gmail.com>
2025-06-09 02:43:14 -0300

    fix: Tipo do usuario sendo passado no jtw.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from users.serializers.userSerializer import CustomTokenObtainPairSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 # Create your views here.
